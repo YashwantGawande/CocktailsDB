125bc1367bd22364ccbad3fbf35458393f39f886
YashwantNG <gawandeyashwant7@gmail.com>
2023-01-22 22:04:53 +0530

    intial commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae expedita facilis pariatur dicta in laborum voluptatum sequi autem, harum aperiam dolore odio dignissimos hic vitae officiis minima laboriosam ea repellendus!
+      Voluptate, voluptatibus unde! Voluptatum, commodi doloribus quam amet nostrum mollitia aliquam perspiciatis veniam quod repellat nobis harum sed hic tenetur quo rerum! In nobis consectetur hic autem alias? Vero, qui.
+      Architecto reiciendis iure deserunt, quaerat blanditiis non ducimus eaque dicta voluptas! Debitis rerum cum quasi. Consequuntur consequatur unde possimus soluta delectus nisi, ipsum labore quae dolores, similique sunt praesentium. Eius.
+      Ea impedit consequuntur voluptatibus blanditiis incidunt, iste temporibus harum sequi at accusamus. Perferendis rem unde officia ipsum quos sed, reprehenderit doloribus beatae incidunt doloremque ullam aperiam non praesentium. Blanditiis, labore.</p>
+    </section>
   )
 }
 
